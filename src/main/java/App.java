2806394c0beaa435fb6ee32e7e0675ecff40999d
JoseSilva1997 <carlitoscaba16@gmail.com>
2026-05-22 19:25:26 +0100

    Improved sheet selector dialog

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,10 @@
+import com.formdev.flatlaf.FlatLightLaf;
+
 import gui.Gui;
 
 public class App {
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         Gui.launch();
     }
 }
